86c39e8f73536bcc7d3fd799e0ee1a3ed2dafeb3
Kondaparthy Pavan Kumar <pavankumar@Kondaparthys-MacBook-Pro.local>
2020-09-14 09:05:48 +0530

    es6 - let and constant declarations

---
+++ javascript/day12/server.js
@@ -0,0 +1,15 @@
+function apiCall(resolve, reject, method, url){
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    
+    xhr.addEventListener('readystatechange', function(item){
+        if(xhr.readyState == 4){
+            if(xhr.status >= 200 && xhr.status < 300){
+                resolve(JSON.parse(item.target.responseText));
+            }else{
+                reject(item);
+            }
+        }
+    })
+    xhr.send();
+}
