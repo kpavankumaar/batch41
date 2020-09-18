4813c559237041eef5f43aa2cdba09405f887ba2
Kondaparthy Pavan Kumar <pavankumar@Kondaparthys-MacBook-Pro.local>
2020-09-18 08:07:15 +0530

    Observables

--- javascript/day12/server.js
+++ javascript/day12/server.js
@@ -5,6 +5,7 @@ function apiCall(resolve, reject, method, url){
     xhr.addEventListener('readystatechange', function(item){
         if(xhr.readyState == 4){
             if(xhr.status >= 200 && xhr.status < 300){
+                
                 resolve(JSON.parse(item.target.responseText));
             }else{
                 reject(item);
